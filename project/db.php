f36adcb22545278f3f70583c88f3f694df4a90d4
Ll44052 <Ll44052@mail.ru>
2025-05-28 21:20:52 +0300

    Update db.php

--- project/db.php
+++ project/db.php
@@ -1,9 +1,9 @@
 <?php
 //файл соединения с базой данных mysql
 $host   = 'localhost';
-$dbname = 'u68687';
-$user   = 'u68687';
-$pass   = '8401584';
+$dbname = 'u68676';
+$user   = 'u68676';
+$pass   = '8999741';
 //Подключаемся к базе
 $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
